8d1ed828400c7112076fecd1ed8708694fa54abe
rogerbf <roger@bergfalk.nu>
2016-11-10 22:32:31 +0100

    fail faster, refactor

--- source/index.js
+++ source/index.js
@@ -1,23 +1,17 @@
 import { execFile } from 'child_process'
 
-export default (
-  command = undefined,
-  options = {
-    maxBuffer: 1000 * 1000 * 10
-  }
-) =>
-  (args = [], options = options) =>
-    new Promise(
-      (resolve, reject) =>
-        execFile(command, [].concat(args), options, (err, stdout, stderr) => {
-          if (err) {
-            stderr.length > 0
-            ? reject(stderr.trim())
-            : reject(err)
-          } else {
-            stdout.length > 0
-            ? resolve(stdout.toString().trim())
-            : resolve()
-          }
-        })
-    )
+const defaultOptions = { maxBuffer: 1000 * 1000 * 10 }
+
+export default (command = undefined, options = defaultOptions) => {
+  if (!command) { throw new Error(`missing argument: command`) }
+
+  return (args = [], options = options) => new Promise((resolve, reject) =>
+    execFile(command, [].concat(args), options, (err, stdout, stderr) => {
+      if (err) {
+        stderr.length > 0 ? reject(stderr.toString().trim()) : reject(err)
+      } else {
+        stdout.length > 0 ? resolve(stdout.toString().trim()) : resolve()
+      }
+    })
+  )
+}
