19dd93cb37e33409a756d9a7b9829595088fdbc1
rogerbf <roger@bergfalk.nu>
2017-03-26 23:35:34 +0200

    export options-to-args module

--- source/index.js
+++ source/index.js
@@ -2,4 +2,7 @@ const { execFile } = require(`child_process`)
 const args = require(`options-to-args`)
 const boundExecFile = require(`./bound-execfile`)
 
-module.exports = boundExecFile.bind(null, { execFile, args })
+module.exports = Object.assign(
+  boundExecFile.bind(null, { execFile, args }),
+  args
+)
