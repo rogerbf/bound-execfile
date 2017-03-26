d13901aa41403c179d45767eec8b42b37ef0add5
rogerbf <roger@bergfalk.nu>
2017-03-26 23:26:15 +0200

    inject dependencies, export bound function

--- source/index.js
+++ source/index.js
@@ -1,1 +1,5 @@
-module.exports = {}
+const { execFile } = require(`child_process`)
+const args = require(`options-to-args`)
+const boundExecFile = require(`./bound-execfile`)
+
+module.exports = boundExecFile.bind(null, { execFile, args })
